7afdc938810e52b59f6220b7e07afa0e891f7e22
Matheos Mattsson <matheos.mattsson@gmail.com>
2024-01-20 23:47:47 +0200

    Update .NET packages and refactor name

--- PidroCounter.NET/Extensions/JsRuntimeExtensions.cs
+++ PidroCounter.NET/Extensions/JsRuntimeExtensions.cs
@@ -2,7 +2,7 @@ namespace PidroCounter.NET.Extensions;
 
 using Microsoft.JSInterop;
 
-public static class JRuntimesExtensions
+public static class JsRuntimeExtensions
 {
     internal static void SetLocalStorage(this IJSRuntime js, string key, string value) => ((IJSInProcessRuntime)js).InvokeVoid("localStorage.setItem", key, value);
     internal static string GetLocalStorage(this IJSRuntime js, string key) => ((IJSInProcessRuntime)js).Invoke<string>("localStorage.getItem", key);
